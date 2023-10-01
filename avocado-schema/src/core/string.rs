90780d928706761c9272d4c17c0f829a9c484b48
Wei Zheng <zwnormal@gmail.com>
2023-10-01 15:41:26 +1100

    Add builder for string field

--- avocado-schema/src/core/string.rs
+++ avocado-schema/src/core/string.rs
@@ -5,6 +5,7 @@ use crate::core::constraint::string::max_length::MaxLength;
 use crate::core::constraint::string::min_length::MinLength;
 use crate::core::constraint::string::pattern::Pattern;
 use crate::core::constraint::Constraint;
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -50,33 +51,82 @@ impl Field for StringField {
     }
 }
 
+#[derive(Default)]
+pub struct StringFieldBuilder {
+    name: String,
+    title: String,
+    enumeration: Option<Vec<String>>,
+    max_length: Option<usize>,
+    min_length: Option<usize>,
+    pattern: Option<Regex>,
+}
+
+impl StringFieldBuilder {
+    pub fn new() -> Self {
+        StringFieldBuilder::default()
+    }
+
+    pub fn name(mut self, name: &'static str) -> Self {
+        self.name = name.to_string();
+        self
+    }
+
+    pub fn title(mut self, title: &'static str) -> Self {
+        self.title = title.to_string();
+        self
+    }
+
+    pub fn enumeration(mut self, strings: Vec<String>) -> Self {
+        self.enumeration = Some(strings);
+        self
+    }
+
+    pub fn max_length(mut self, length: usize) -> Self {
+        self.max_length = Some(length);
+        self
+    }
+
+    pub fn min_length(mut self, length: usize) -> Self {
+        self.min_length = Some(length);
+        self
+    }
+
+    pub fn pattern(mut self, pattern: Regex) -> Self {
+        self.pattern = Some(pattern);
+        self
+    }
+
+    pub fn build(self) -> StringField {
+        StringField {
+            name: self.name,
+            title: self.title,
+            enumeration: self.enumeration.map(|values| Enumeration { values }),
+            max_length: self.max_length.map(|max_length| MaxLength { max_length }),
+            min_length: self.min_length.map(|min_length| MinLength { min_length }),
+            pattern: self.pattern.map(|pattern| Pattern { pattern }),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::core::constraint::string::enumeration::Enumeration;
-    use crate::core::constraint::string::max_length::MaxLength;
-    use crate::core::constraint::string::min_length::MinLength;
-    use crate::core::constraint::string::pattern::Pattern;
-    use crate::core::string::StringField;
+    use crate::core::string::{StringField, StringFieldBuilder};
     use regex::Regex;
 
     #[test]
     fn test_serialize() {
-        let field = StringField {
-            name: "body".to_string(),
-            title: "Body".to_string(),
-            enumeration: Some(Enumeration {
-                values: vec!["meeting".to_string(), "email".to_string()],
-            }),
-            max_length: Some(MaxLength { max_length: 32 }),
-            min_length: Some(MinLength { min_length: 8 }),
-            pattern: Some(Pattern {
-                pattern: Regex::new(r"[a-z]+").unwrap(),
-            }),
-        };
+        let field = StringFieldBuilder::new()
+            .name("subtype")
+            .title("SubType")
+            .enumeration(vec!["meeting".to_string(), "email".to_string()])
+            .max_length(32)
+            .min_length(8)
+            .pattern(Regex::new(r"[a-z]+").unwrap())
+            .build();
         let field_json = serde_json::to_string(&field).unwrap();
         assert_eq!(
             field_json,
-            r#"{"type":"string","name":"body","title":"Body","enum":["meeting","email"],"maxLength":32,"minLength":8,"pattern":"[a-z]+"}"#
+            r#"{"type":"string","name":"subtype","title":"SubType","enum":["meeting","email"],"maxLength":32,"minLength":8,"pattern":"[a-z]+"}"#
         );
     }
 
@@ -85,16 +135,16 @@ mod tests {
         let field_json = r#"
         {
             "type":"string",
-            "name": "type",
-            "title": "Type",
+            "name": "subtype",
+            "title": "SubType",
             "enum": ["meeting", "email"],
             "maxLength": 32,
             "minLength": 8,
             "pattern": "[a-z]+"
         }"#;
         let field: StringField = serde_json::from_str(field_json).unwrap();
-        assert_eq!(field.name, "type");
-        assert_eq!(field.title, "Type");
+        assert_eq!(field.name, "subtype");
+        assert_eq!(field.title, "SubType");
         assert_eq!(field.enumeration.unwrap().values, vec!["meeting", "email"]);
         assert_eq!(field.max_length.unwrap().max_length, 32);
         assert_eq!(field.min_length.unwrap().min_length, 8);
