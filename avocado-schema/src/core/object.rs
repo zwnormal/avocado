b656a4359844d0aa0be6e1303333280c5b4e35d5
Wei Zheng <zwnormal@gmail.com>
2023-10-04 07:28:32 +1100

    Move test deserliase object to ObjectField

--- avocado-schema/src/core/object.rs
+++ avocado-schema/src/core/object.rs
@@ -87,3 +87,60 @@ impl ObjectFieldBuilder {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::core::object::ObjectField;
+
+    #[test]
+    fn test_deserialize() {
+        let valid_schema_json = r#"
+        {
+            "type": "object",
+            "name": "client",
+            "title": "Client",
+            "properties": {
+                "first_name": {
+                    "type": "string",
+                    "name": "first_name",
+                    "title": "First Name",
+                    "maxLength": 32,
+                    "minLength": 8
+                },
+                "last_name": {
+                    "type": "string",
+                    "name": "last_name",
+                    "title": "Last Name",
+                    "maxLength": 32,
+                    "minLength": 8,
+                    "pattern": "[a-zA-Z]+"
+                }
+            }
+        }"#;
+        assert!(serde_json::from_str::<ObjectField>(valid_schema_json).is_ok());
+
+        let invalid_schema_json = r#"
+        {
+            "type": "object",
+            "name": "client",
+            "title": "Client",
+            "properties": {
+                "first_name": {
+                    "type": "string",
+                    "name": "first_name",
+                    "title": "First Name",
+                    "maxLength": 32,
+                    "minLength": -1,
+                },
+                "last_name": {
+                    "type": "string",
+                    "name": "last_name",
+                    "title": "Last Name",
+                    "maxLength": 32,
+                    "minLength": 8
+                }
+            }
+        }"#;
+        assert!(serde_json::from_str::<ObjectField>(invalid_schema_json).is_err());
+    }
+}
